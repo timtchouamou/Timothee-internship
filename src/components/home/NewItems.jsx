3c1936a257f9dd3782940c5dde5b347d6d345f7b
timtchouamou <tim.tchouamou@gmail.com>
2025-10-08 03:20:25 -0400

    New Items Pratice

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import SkeletonAll from "../UI/SkeletonAll"; // ✅ Import adaptive skeleton
+
 
 const NewItems = () => {
+
+  const [card, setCard] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchNewItems() {
+    
+      const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems");
+      console.log(data)
+      setCard(data);
+      setTimeout(() => setLoading(false),4000); 
+  }
+
+  useEffect(() => {
+    fetchNewItems();
+  }, []);
+
+  // Countdown formatter
+  const formatTime = (seconds) => {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+    return `${h}h ${m}m ${s}s`;
+  };
+
+  const [timeLeft, setTimeLeft] = useState([]);
+
+  useEffect(() => {
+    if (!loading && card.length > 0) {
+      const countdowns = card.map((item) => {
+        if (item.expiryDate) {
+          return Math.floor((new Date(item.expiryDate) - new Date()) / 1000);
+        }
+        
+        return 5 * 3600 + 30 * 60 + 32; // fallback
+      });
+
+      setTimeLeft(countdowns);
+
+      const interval = setInterval(() => {
+        setTimeLeft((prev) => prev.map((time) => (time > 0 ? time - 1 : 0)));
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [loading, card]);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -13,63 +65,68 @@ const NewItems = () => {
               <h2>New Items</h2>
               <div className="small-border bg-color-2"></div>
             </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            loop={true}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              1024: { slidesPerView: 4 },
+              768: { slidesPerView: 2 },
+              0: { slidesPerView: 1 },
+            }}
+          >
+
+        {loading ? 
+          
+            Array.from({ length: 8 }).map((_, index) => (
+               <SwiperSlide key={index}>
+             <SkeletonAll type="item" />
+              </SwiperSlide>
+            ))
+         
+         : 
+          
+            card.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link to={`/author/${item.authorId}`}  title={`Creator: ${item.authorName}`}>
+                      <img className="lazy" src={item.authorImage} alt="" />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <div className="de_countdown">
+                    {timeLeft[index] !== undefined
+                      ? formatTime(timeLeft[index])
+                      : "0h 0m 0s"}
+                  </div>
+                  <div className="nft__item_wrap">
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <img src={item.nftImage} className="lazy nft__item_preview" alt="" />
+                    </Link>
+                  </div>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
                     </div>
                   </div>
+                </div>
+              </SwiperSlide>
+            ))
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          }
+          
+          </Swiper>
+        </div>
         </div>
       </div>
     </section>

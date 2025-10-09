033d2319e4209e0b7df3959e5f046bca76101548
Timothee D TCHOUAMOU <78961183+timtchouamou@users.noreply.github.com>
2025-10-08 20:29:55 -0400

    Update TopSellers.jsx

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -11,7 +11,7 @@ const TopSellers = () => {
     const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`);
     console.log(data);
     setAuthor(data);
-    setTimeout(() => setLoading(false),4000);
+    setTimeout(() => setLoading(false),2000);
 
   }
 
